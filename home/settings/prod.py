83d512a69474363ae79ebdeaffb09411cc4239c5
DataFluent Analytics <dfa@DataFluents-MacBook-Air.local>
2022-05-21 17:21:29 +0600

    api prod

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -60,23 +60,12 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Allow all host headers
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['https://df-api-islam.herokuapp.com']
     DEBUG = True
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-    AWS_QUERYSTRING_AUTH = False
-    AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "")
+    
 
 # Static asset configuration
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 django_heroku.settings(locals())
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-MEDIA_ROOT = os.environ.get("MEDIA_URL", "static_files/image")
-
-# MEDIA_URL = '/image/'
-
-SENDFILE_BACKEND = 'sendfile.backends.simple'

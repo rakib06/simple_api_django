f1474bcb9194d64ed9e75d23b1262572d7ba4e8a
DataFluent Analytics <dfa@DataFluents-MacBook-Air.local>
2022-05-21 17:24:31 +0600

    api prod

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -1,9 +1,8 @@
 '''Use this for production'''
-from .local import *
 import django_heroku
 from .base import *
 import dj_database_url
-# from home.aws.conf import *
+
 
 # WSGI_APPLICATION = 'home.wsgi.prod.application'
 WSGI_APPLICATION = 'home.wsgi.prod.application'
@@ -15,7 +14,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DEBUG = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -35,22 +34,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 MIDDLEWARE += [
-    'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+    'whitenoise.middleware.WhiteNoiseMiddleware']
 
 
 '''
-https://rk-mcq.herokuapp.com// | https://git.heroku.com/rkmcq.git
+https://exam-player.com// | https://git.heroku.com/rkmcq.git
 '''
-
-STRIPE_SECRET_KEY = ''
-
-
 # for heroku
 if os.getcwd() == '/app':
     db_from_env = dj_database_url.config(conn_max_age=500)
@@ -60,12 +54,10 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Allow all host headers
-    ALLOWED_HOSTS = ['https://df-api-islam.herokuapp.com']
+    ALLOWED_HOSTS = ['rk-mcq.herokuapp.com', 'exam-player.com']
     DEBUG = True
-    
 
-# Static asset configuration
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 django_heroku.settings(locals())
-

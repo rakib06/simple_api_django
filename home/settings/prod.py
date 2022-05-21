e84e16ef6e439a40163d3453718bc1d72a56ba2f
DataFluent Analytics <dfa@DataFluents-MacBook-Air.local>
2022-05-21 17:30:36 +0600

    bug

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -54,7 +54,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Allow all host headers
-    ALLOWED_HOSTS = ['rk-mcq.herokuapp.com', 'exam-player.com']
+    ALLOWED_HOSTS = ['df-api-islam.herokuapp.com']
     DEBUG = True
 
     # Static asset configuration
